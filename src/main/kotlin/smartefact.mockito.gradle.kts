044ab07ebeba7e6aca220263d659e2be92221961
Laurent Pireyn <laurent.pireyn@smartefact.be>
2026-08-26 21:44:40 +0200

    Create `smartefact.mockito` convention

---
+++ src/main/kotlin/smartefact.mockito.gradle.kts
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * Smartefact Gradle conventions - Mockito
+ *
+ * Uses Mockito in all JVM test suites.
+ *
+ * Reference: https://site.mockito.org/
+ *
+ * @author Laurent Pireyn
+ */
+
+plugins {
+    id("smartefact.junit-platform")
+}
+
+val mockitoVersion = "5.22.0"
+
+testing {
+    jvmTestSuites {
+        dependencies {
+            implementation(platform("org.mockito:mockito-bom:$mockitoVersion"))
+            implementation("org.mockito:mockito-core")
+        }
+    }
+}
