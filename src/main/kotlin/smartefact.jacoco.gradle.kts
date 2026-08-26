45735a106cff2d45c89409ba11894bbbe8015d46
Laurent Pireyn <laurent.pireyn@smartefact.be>
2026-08-26 21:42:28 +0200

    Create `smartefact.jacoco` convention

---
+++ src/main/kotlin/smartefact.jacoco.gradle.kts
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * Smartefact Gradle conventions - JaCoCo
+ *
+ * Generates JaCoCo reports for the unit tests.
+ *
+ * Reference: https://www.jacoco.org/jacoco/
+ * Gradle plugin: https://docs.gradle.org/current/userguide/jacoco_plugin.html
+ *
+ * @author Laurent Pireyn
+ */
+
+plugins {
+    id("smartefact.java")
+    id("jacoco")
+}
+
+val jacocoVersion = "0.8.14"
+
+jacoco {
+    toolVersion = jacocoVersion
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+        html.required = true
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
