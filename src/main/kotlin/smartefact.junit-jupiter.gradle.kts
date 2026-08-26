91cbb9cacc16b8e6d2bbc167ae556e70d19c8aef
Laurent Pireyn <laurent.pireyn@smartefact.be>
2026-08-26 21:28:15 +0200

    Create `smartefact.junit-jupiter` convention

---
+++ src/main/kotlin/smartefact.junit-jupiter.gradle.kts
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * Smartefact Gradle conventions - JUnit Jupiter
+ *
+ * Uses JUnit Jupiter in all JVM test suites.
+ *
+ * @author Laurent Pireyn
+ */
+
+plugins {
+    id("smartefact.junit-platform")
+}
+
+testing {
+    jvmTestSuites {
+        dependencies {
+            implementation("org.junit.jupiter:junit-jupiter")
+            implementation("org.junit.jupiter:junit-jupiter-params")
+        }
+    }
+}
