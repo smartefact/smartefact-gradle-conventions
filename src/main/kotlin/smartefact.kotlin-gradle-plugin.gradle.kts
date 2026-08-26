bb30476bb072e323d4f47799a28798ecfc78e1f9
Laurent Pireyn <laurent.pireyn@smartefact.be>
2026-08-26 22:04:32 +0200

    Create `smartefact.kotlin-gradle-plugin` convention

---
+++ src/main/kotlin/smartefact.kotlin-gradle-plugin.gradle.kts
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * Smartefact Gradle conventions - Kotlin Gradle plugin
+ *
+ * @author Laurent Pireyn
+ */
+
+plugins {
+    id("smartefact.java-gradle-plugin")
+    id("smartefact.kotlin-jvm")
+}
