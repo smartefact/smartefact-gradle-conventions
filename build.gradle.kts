5f34e09b1bd1225d132caf4e4dfd137d95cdd46f
Laurent Pireyn <laurent.pireyn@smartefact.be>
2026-08-26 22:15:37 +0200

    Create `smartefact.jmh` convention

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ description = "Gradle conventions for Smartefact projects."
 dependencies {
     api("biz.aQute.bnd.builder:biz.aQute.bnd.builder.gradle.plugin:7.2.1")
     api("com.gradle.plugin-publish:com.gradle.plugin-publish.gradle.plugin:2.1.1")
+    api("me.champeau.jmh:me.champeau.jmh.gradle.plugin:0.7.3")
     api("net.ltgt.gradle:gradle-errorprone-plugin:5.1.0")
     api("net.ltgt.gradle:gradle-nullaway-plugin:3.0.0")
     api("org.jetbrains.dokka:dokka-gradle-plugin:2.2.0")
---
+++ src/main/kotlin/smartefact.jmh.gradle.kts
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * Smartefact Gradle conventions - JMH
+ *
+ * Reference: https://github.com/openjdk/jmh
+ * Gradle plugin: https://plugins.gradle.org/plugin/me.champeau.jmh
+ *
+ * @author Laurent Pireyn
+ */
+
+plugins {
+    id("smartefact.java")
+    id("me.champeau.jmh")
+}
+
+val theJmhVersion = "1.37"
+
+jmh {
+    jmhVersion = theJmhVersion
+    resultFormat = "CSV"
+    // TODO: Configure Async Profiler
+}
