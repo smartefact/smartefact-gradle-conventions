597e17bf026bfbd6e57a899c1a1321fbd07d65c9
Laurent Pireyn <laurent.pireyn@smartefact.be>
2026-08-26 21:29:07 +0200

    Create `smartefact.junit-pioneer` convention

---
+++ src/main/kotlin/smartefact.junit-pioneer.gradle.kts
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+/**
+ * Smartefact Gradle conventions - JUnit Pioneer
+ *
+ * Uses JUnit Pioneer in all JVM test suites.
+ *
+ * Reference: https://junit-pioneer.org/
+ *
+ * @author Laurent Pireyn
+ */
+
+plugins {
+    id("smartefact.junit-platform")
+}
+
+val junitpioneerVersion = "2.3.0"
+
+testing {
+    jvmTestSuites {
+        dependencies {
+            implementation("org.junit-pioneer:junit-pioneer:$junitpioneerVersion")
+        }
+    }
+}
