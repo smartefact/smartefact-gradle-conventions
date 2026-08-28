4d3f77cb49d3bb4fe8eb43a48dcd1ca8d1f3b155
Laurent Pireyn <laurent.pireyn@smartefact.be>
2026-08-28 12:19:46 +0200

    Configure plugins publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 
 plugins {
     `kotlin-dsl`
+    id("com.gradle.plugin-publish") version "2.1.1"
 }
 
 group = "org.smartefact"
@@ -16,6 +17,91 @@ dependencies {
     api("net.ltgt.gradle:gradle-nullaway-plugin:3.0.0")
     api("org.jetbrains.dokka:dokka-gradle-plugin:2.2.0")
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+}
+
+gradlePlugin {
+    website = "https://smartefact.org/"
+    vcsUrl = "https://github.com/smartefact/smartefact-gradle-conventions"
+    plugins {
+        // Plugins (alphabetically)
+        getByName("org.smartefact.arch-tests") {
+            displayName = "Smartefact Gradle conventions - Architecture tests"
+        }
+        getByName("org.smartefact.archunit-junit") {
+            displayName = "Smartefact Gradle conventions - ArchUnit on JUnit"
+        }
+        getByName("org.smartefact.base") {
+            displayName = "Smartefact Gradle conventions - Base"
+        }
+        getByName("org.smartefact.checkstyle") {
+            displayName = "Smartefact Gradle conventions - Checkstyle"
+        }
+        getByName("org.smartefact.errorprone") {
+            displayName = "Smartefact Gradle conventions - Error Prone"
+        }
+        getByName("org.smartefact.functional-tests") {
+            displayName = "Smartefact Gradle conventions - Functional tests"
+        }
+        getByName("org.smartefact.gradle-plugin-publish") {
+            displayName = "Smartefact Gradle conventions - Gradle plugin publish"
+        }
+        getByName("org.smartefact.integration-tests") {
+            displayName = "Smartefact Gradle conventions - Integration tests"
+        }
+        getByName("org.smartefact.jacoco") {
+            displayName = "Smartefact Gradle conventions - JaCoCo"
+        }
+        getByName("org.smartefact.java") {
+            displayName = "Smartefact Gradle conventions - Java"
+        }
+        getByName("org.smartefact.java21") {
+            displayName = "Smartefact Gradle conventions - Java 21"
+        }
+        getByName("org.smartefact.java-gradle-plugin") {
+            displayName = "Smartefact Gradle conventions - Java Gradle plugin"
+        }
+        getByName("org.smartefact.java-library") {
+            displayName = "Smartefact Gradle conventions - Java library"
+        }
+        getByName("org.smartefact.jmh") {
+            displayName = "Smartefact Gradle conventions - JMH"
+        }
+        getByName("org.smartefact.junit-jupiter") {
+            displayName = "Smartefact Gradle conventions - JUnit Jupiter"
+        }
+        getByName("org.smartefact.junit-pioneer") {
+            displayName = "Smartefact Gradle conventions - JUnit Pioneer"
+        }
+        getByName("org.smartefact.junit-platform") {
+            displayName = "Smartefact Gradle conventions - JUnit Platform"
+        }
+        getByName("org.smartefact.kotlin-gradle-plugin") {
+            displayName = "Smartefact Gradle conventions - Kotlin Gradle plugin"
+        }
+        getByName("org.smartefact.kotlin-jvm") {
+            displayName = "Smartefact Gradle conventions - Kotlin - JVM"
+        }
+        getByName("org.smartefact.maven-publish") {
+            displayName = "Smartefact Gradle conventions - Maven publish"
+        }
+        getByName("org.smartefact.mockito") {
+            displayName = "Smartefact Gradle conventions - Mockito"
+        }
+        getByName("org.smartefact.mockito-junit-jupiter") {
+            displayName = "Smartefact Gradle conventions - JUnit Jupiter extension for Mockito"
+        }
+        getByName("org.smartefact.nullaway") {
+            displayName = "Smartefact Gradle conventions - NullAway"
+        }
+        getByName("org.smartefact.osgi-bundle") {
+            displayName = "Smartefact Gradle conventions - OSGi bundle"
+        }
+        // Common plugin properties
+        configureEach {
+            description = displayName
+            tags = setOf("conventions")
+        }
+    }
 }
 
 val functionalTest = testing.suites.register<JvmTestSuite>("functionalTest") {
